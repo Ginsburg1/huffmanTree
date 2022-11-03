9e24d0a93948fa5034dd2ee88316a2f884d521c2
Ginsburg1 <99150649+Ginsburg1@users.noreply.github.com>
2022-11-03 10:24:16 -0600

    Delete 2355 directory

--- 2355/hcompress.h
+++
@@ -1,5 +0,0 @@
-huffmanTree* createFreqTable(char* name);
-huffmanTree* createHuffmanTree(huffmanTree* leafNodes);
-void encodeFile(char* filename, huffmanTree* leafNodes);
-void decodeFile(char* filename, huffmanTree* treeRoot);
-
